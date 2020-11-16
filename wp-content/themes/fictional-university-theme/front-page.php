4b2eaccf808bd66af754dcb009f696c8704cb239
Reynald Ramirez <soyreynald@outlook.com>
2020-11-15 22:11:33 -0400

    Sorting a custom query with WP_Query function and PHP

--- wp-content/themes/fictional-university-theme/front-page.php
+++ wp-content/themes/fictional-university-theme/front-page.php
@@ -15,26 +15,46 @@
         <div class="full-width-split__inner">
           <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
           <?php 
+          $today = date('Ymd');
           $homepageEvents = new WP_Query(array(
-            'posts_per_page' => 2,
-            'post_type' => 'event'
+            'posts_per_page' => -1,
+            'post_type' => 'event',
+            'meta_key' => 'event_date',
+            'orderby' => 'meta_value_num',
+            'order' => 'ASC',
+            'meta_query' => array(
+              array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+              )
+            )
+            
           ));
 
           while($homepageEvents->have_posts()):
             $homepageEvents->the_post();
+            $eventsDate = new DateTime(get_field('event_date'));
           ?>
               <div class="event-summary">
                 <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                  <span class="event-summary__month"><?php the_time('M'); ?></span>
-                  <span class="event-summary__day"><?php the_time('d'); ?></span>
+                  <span class="event-summary__month"><?php echo $eventsDate->format('M'); ?></span>
+                  <span class="event-summary__day"><?php echo $eventsDate->format('d'); ?></span>
                 </a>
                 <div class="event-summary__content">
                   <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                  <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                  <p><?php 
+                    if(has_excerpt()){
+                      the_excerpt();
+                    }else{
+                      echo wp_trim_words(get_the_content(), 18); 
+                    }
+                  ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
                 </div>
               </div>
           <?php endwhile; ?>
-          <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+          <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
         </div>
       </div>
       <div class="full-width-split__two">
@@ -55,7 +75,13 @@
               </a>
               <div class="event-summary__content">
                 <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+                <p><?php 
+                if(has_excerpt()){
+                  the_excerpt();
+                }else{
+                  echo wp_trim_words(get_the_content(), 18); 
+                }
+                ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
               </div>
             </div>
           <?php  
