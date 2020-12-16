bd08cb72f33e61cb4f2352f58ff16799c276ddbe
Reynald Ramirez <soyreynald@outlook.com>
2020-12-15 21:10:50 -0400

    Related Programs relationship to an event

--- wp-content/themes/fictional-university-theme/single-program.php
+++ wp-content/themes/fictional-university-theme/single-program.php
@@ -23,9 +23,54 @@
               <span class="metabox__main"><?php the_title(); ?></span></p>
         </div>
 
-        <div class="generic-content">
-            <?php the_content(); ?>
-        </div>
+        <div class="generic-content"><?php the_content(); ?></div>
+
+        <?php 
+          $today = date('Ymd');
+          $homepageEvents = new WP_Query(array(
+            'posts_per_page' => -1,
+            'post_type' => 'event',
+            'meta_key' => 'event_date',
+            'orderby' => 'meta_value_num',
+            'order' => 'ASC',
+            'posts_per_page' => 2,
+            'meta_query' => array(
+              array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+              ),
+              array(
+                  'key' => 'related_programs',
+                  'compare' => 'LIKE',
+                  'value' => '"'. get_the_ID() .'"'
+              )
+            )
+            
+          ));
+
+          while($homepageEvents->have_posts()):
+            $homepageEvents->the_post();
+            $eventsDate = new DateTime(get_field('event_date'));
+          ?>
+              <div class="event-summary">
+                <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+                  <span class="event-summary__month"><?php echo $eventsDate->format('M'); ?></span>
+                  <span class="event-summary__day"><?php echo $eventsDate->format('d'); ?></span>
+                </a>
+                <div class="event-summary__content">
+                  <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                  <p><?php 
+                    if(has_excerpt()){
+                      the_excerpt();
+                    }else{
+                      echo wp_trim_words(get_the_content(), 18); 
+                    }
+                  ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                </div>
+              </div>
+          <?php endwhile; ?>
     </div>
 <?php 
     endwhile;
