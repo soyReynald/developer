fa1ad15660d215d46c4391ee9ce1bc293a536e03
Reynald Ramirez <soyreynald@outlook.com>
2020-12-20 14:08:28 -0400

    Quick edits

--- wp-content/themes/fictional-university-theme/single-program.php
+++ wp-content/themes/fictional-university-theme/single-program.php
@@ -49,28 +49,35 @@
             )
             
           ));
+          if($homepageEvents->have_posts()){
 
-          while($homepageEvents->have_posts()):
-            $homepageEvents->the_post();
-            $eventsDate = new DateTime(get_field('event_date'));
-          ?>
-              <div class="event-summary">
-                <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                  <span class="event-summary__month"><?php echo $eventsDate->format('M'); ?></span>
-                  <span class="event-summary__day"><?php echo $eventsDate->format('d'); ?></span>
-                </a>
-                <div class="event-summary__content">
-                  <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                  <p><?php 
-                    if(has_excerpt()){
-                      the_excerpt();
-                    }else{
-                      echo wp_trim_words(get_the_content(), 18); 
-                    }
-                  ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+            echo "<hr class='section-break'>";
+            echo "<h2 class='headline headline--medium'>Upcoming ". get_the_title() . " Events</h2>";
+
+            while($homepageEvents->have_posts()):
+              $homepageEvents->the_post();
+              $eventsDate = new DateTime(get_field('event_date'));
+            ?>
+                <div class="event-summary">
+                  <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+                    <span class="event-summary__month"><?php echo $eventsDate->format('M'); ?></span>
+                    <span class="event-summary__day"><?php echo $eventsDate->format('d'); ?></span>
+                  </a>
+                  <div class="event-summary__content">
+                    <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                    <p><?php 
+                      if(has_excerpt()){
+                        the_excerpt();
+                      }else{
+                        echo wp_trim_words(get_the_content(), 18); 
+                      }
+                    ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                  </div>
                 </div>
-              </div>
-          <?php endwhile; ?>
+            <?php endwhile; ?>
+          <?php
+          }
+          ?>
     </div>
 <?php 
     endwhile;
